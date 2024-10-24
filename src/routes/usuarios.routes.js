883711ca900d9a6037fecf8040ae167c27573585
anna leme <annaleme.tds1.senai@gmail.com>
2024-10-24 11:39:15 -0300

    feat: create get all users

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,4 +4,14 @@ const usuariosRoutes = Router();
 
 let usuarios = [];
 
+usuariosRoutes.get("/", (req, res)=> {
+    return res.status(200).json({
+        message:
+        usuarios.lenght == 0 
+        ? "Não há usuários cadastrados"
+        : `Total de usuarios: ${usuarios.length}`,
+        usuarios,
+    })
+})
+
 export default usuariosRoutes;
