668b14e806f972030170e25649d89560ace7d5b7
anna leme <annaleme.tds1.senai@gmail.com>
2024-10-24 11:20:35 -0300

    refactor adjust index routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import usuariosRoutes from "./usuarios.routes.js";
 
 const routes = Router();
 
@@ -6,5 +7,7 @@ const routes = Router();
 routes.get("/", (req, res) => {
 return res.status(200).json({ message: "Vai São Paulooooooo!" });
 });
+
+routes.use("/usuarios", usuariosRoutes);
 
 export default routes;
